6bceeb8d6a49b7aac7db0c38fc1a4f113a70956b
Kosmos Digital <hello@kosmos-digital.com>
2021-01-08 14:05:32 +0100

    push

--- lib/ag_lib.dart
+++ lib/ag_lib.dart
@@ -2,9 +2,4 @@ library ag_lib;
 
 /* MainAppController */
 
-export "mainAppController/body.dart";
-export "mainAppController/bottomNavBar.dart";
-export "mainAppController/mainAppControllerLoader.dart";
-export "mainAppController/navBar.dart";
-export "mainAppController/navBarItems.dart";
-export "mainAppController/unexpected.dart";
+export "mainAppController/mainAppController.dart";
---
+++ lib/mainAppController/mainAppController.dart
@@ -0,0 +1,6 @@
+export "body.dart";
+export "bottomNavBar.dart";
+export "mainAppControllerLoader.dart";
+export "navBar.dart";
+export "navBarItems.dart";
+export "unexpected.dart";
